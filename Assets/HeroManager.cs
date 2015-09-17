4bdb581580b1c596b093d05f9ccf7c999961221d
David YANA <david.yana@yahoo.fr>
2015-09-17 14:56:04 +0200

    creation des dossiers

---
+++ Assets/HeroManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HeroManager : MonoBehaviour {
+
+    Hero h;
+    List<Monster> lm;
+	// Use this for initialization
+	void Start () {
+        h = new Hero(20);	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

6718574a0848c6e326310b8820217944d80be3a9
Cricroo <crizz13@hotmail.fr>
2015-09-17 23:03:24 +0200

    Stuff

---
+++ Assets/Scripts/Classes/Stuff.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stuff
+{
+    /* Stuff Variable */
+    private int attackValues;
+    private int attackSpeed;
+    private int armor;
+    Sprite icon;
+    string name;
+    string description;
+    BonusType strenBonus, intelBonus, agiBonus, healthBonus, spellBonus, critBonus, dodgeBonus, regenHealthBonus, regenManaBonus, attackBonus, manaBonus;
+    
+    /* Stuff Enum */
+    enum BonusType { strenBonus, intelBonus, agiBonus, healthBonus, spellBonus, critBonus, dodgeBonus, regenHealthBonus, regenManaBonus, attackBonus, manaBonus }
+    enum SlotType { head, oneHand, twoHands, feet, chest, legs, hands };
+       
+}

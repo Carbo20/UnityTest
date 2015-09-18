da56aa7d488bdf4b7de7792c7a220396c958567f
Lucky <dufau.luc@gmail.com>
2015-09-18 14:22:01 +0200

    vla un commentaire : item

---
+++ Assets/Scripts/Classes/Item.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item
+{
+    /* Item's Variables */
+    private int attackValues;
+    private float attackSpeed;
+    private int armor;
+    Sprite icon;
+    string name;
+    string description;
+
+    int strenBonus, intelBonus, agiBonus, vitalBonus, attackBonus, spellBonus, manaBonus, regenManaBonus, dodgeBonus, critBonus, regenHealthBonus, healthBonus;
+    float attackSpeedBonus, castTimeBonus; // represented by a %, it will reduce the delay after an attack or a cast is done
+
+    Data.SlotType slotType;
+
+    enum BonusType { STREN, INTEL, AGI, VITAL, HEALTH, };
+
+    //public enum SlotType { HEAD, CHEST, HANDS, LEGS, FEET, ONEHAND, TWOHANDS };
+
+    
+    void GenerateItem(int level, int magicFind)
+    {
+        //int rand = UnityEngine.Random.Range(0, max); max exclu  // value au lieu de range pour les float
+        //rand pour determiner quel type d'item va drop : emplacement + type d'item (hache, épée,...)
+        //rand pour determiner quel QUALITE d'item ce sera : Nomral : 0 bonus, magic : 1 à 2 bonus, legendaire 3 à 4 bonus + un effet
+        //rands pour déterminer quels BONUS aura l'item
+        //rands sur chaque bonus en fonction du lvl pour déterminer la VALEUR du bonus
+        //si la qualité de l'item est légendaire on ajoute un effet particulier
+        //on génére le nom de l'objet de manière automatique
+
+        
+        int itemSlotType = UnityEngine.Random.Range(0, 7); // Type de slot d'item /SlotType : HEAD, CHEST, HANDS, LEGS, FEET, ONEHAND, TWOHANDS    ///PB : 2 chance sur 7 d'avoir une arme et 5 / 7 d'avoir une armure
+        Debug.Log("itemSlotType " + itemSlotType);
+        int itemType = UnityEngine.Random.Range(0, 3); // Type de l'item : weapon, armor ou shield
+        int itemQuality = UnityEngine.Random.Range(0, 3); // Qualite de l'item : normal, magic, legendaire
+        Debug.Log("itemQuality " + itemQuality);
+
+
+
+
+    }
+
+
+
+}

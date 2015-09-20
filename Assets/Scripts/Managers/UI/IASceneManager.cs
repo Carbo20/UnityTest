1b8220ae442efb867bec6a0da17476b8f36b940c
Cricroo <crizz13@hotmail.fr>
2015-09-20 17:00:16 +0200

    update is in peace naoo

--- Assets/Scripts/Managers/UI/IASceneManager.cs
+++ Assets/Scripts/Managers/UI/IASceneManager.cs
@@ -7,6 +7,7 @@ public class IASceneManager : MonoBehaviour
 
     private Dropdown categoryDropdown, categoryDropdown1, categoryDropdown2;
     private int indexButtonCat;
+    private int indexButtonCatMax;
     private int indexCatMax;
     private int unchosenCount;
     private Button button;
@@ -15,6 +16,12 @@ public class IASceneManager : MonoBehaviour
     private GameObject[] gObjHealthBis = new GameObject[8];
     private GameObject[] gObjMana = new GameObject[8];
     private GameObject[] gObjManaBis = new GameObject[8];
+
+    private GameObject[] gObjHealthEn = new GameObject[8];
+    private GameObject[] gObjHealthBisEn = new GameObject[8];
+    private GameObject[] gObjManaEn = new GameObject[8];
+    private GameObject[] gObjManaBisEn = new GameObject[8];
+
     private GameObject[] gObjCast = new GameObject[8];
     private GameObject[] gObjEnemiesNb = new GameObject[8];
     private GameObject[] gObjEnemyClass = new GameObject[8];
@@ -27,15 +34,22 @@ public class IASceneManager : MonoBehaviour
 
     void initAll()
     {
-        indexCatMax = 8;
+        indexButtonCatMax = 8;
+        indexCatMax = 12;
         unchosenCount = 0;
 
-        for (int ind = 0; ind < 8; ind++)
+        for (int ind = 0; ind < indexButtonCatMax; ind++)
         {
             gObjHealth[ind].SetActive(false);
             gObjHealthBis[ind].SetActive(false);
             gObjMana[ind].SetActive(false);
             gObjManaBis[ind].SetActive(false);
+
+            gObjHealthEn[ind].SetActive(false);
+            gObjHealthBisEn[ind].SetActive(false);
+            gObjManaEn[ind].SetActive(false);
+            gObjManaBisEn[ind].SetActive(false);
+
             gObjCast[ind].SetActive(false);
             gObjEnemiesNb[ind].SetActive(false);
             gObjEnemyClass[ind].SetActive(false);
@@ -48,6 +62,12 @@ public class IASceneManager : MonoBehaviour
         gObjHealthBis[ind].SetActive(false);
         gObjMana[ind].SetActive(false);
         gObjManaBis[ind].SetActive(false);
+
+        gObjHealthEn[ind].SetActive(false);
+        gObjHealthBisEn[ind].SetActive(false);
+        gObjManaEn[ind].SetActive(false);
+        gObjManaBisEn[ind].SetActive(false);
+
         gObjCast[ind].SetActive(false);
         gObjEnemiesNb[ind].SetActive(false);
         gObjEnemyClass[ind].SetActive(false);
@@ -63,9 +83,16 @@ public class IASceneManager : MonoBehaviour
             gObjHealthBis[ind] = GameObject.Find("CondHealthBis" + ind);
             gObjMana[ind] = GameObject.Find("CondMana" + ind);
             gObjManaBis[ind] = GameObject.Find("CondManaBis" + ind);
+
+            gObjHealthEn[ind] = GameObject.Find("CondHealthEn" + ind);
+            gObjHealthBisEn[ind] = GameObject.Find("CondHealthBisEn" + ind);
+            gObjManaEn[ind] = GameObject.Find("CondManaEn" + ind);
+            gObjManaBisEn[ind] = GameObject.Find("CondManaBisEn" + ind);
+
             gObjCast[ind] = GameObject.Find("CondCast" + ind);
             gObjEnemiesNb[ind] = GameObject.Find("CondEnemiesNb" + ind);
             gObjEnemyClass[ind] = GameObject.Find("CondEnemyClass" + ind);
+
         }
 
 
@@ -76,54 +103,53 @@ public class IASceneManager : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CallbackDropdownChanged(bool isChanged)
     {
-
-
-        for (indexButtonCat = 1; indexButtonCat < indexCatMax; indexButtonCat++)
+        for (indexButtonCat = 1; indexButtonCat < indexButtonCatMax + 1; indexButtonCat++)
         {
             categoryDropdown = GameObject.Find("categoryDropdown(" + indexButtonCat + ")").GetComponent<Dropdown>();
             Debug.Log("categoryDropdown(" + indexButtonCat + ")");
 
             switchCatCount = 0;
-            
+
             //Player's Health
             if (categoryDropdown.value == 1)
             {
-               
-                initLine(indexButtonCat-1);
+
+                initLine(indexButtonCat - 1);
 
                 switchCatCount++;
-                gObjHealth[indexButtonCat-1].SetActive(true);
+                gObjHealth[indexButtonCat - 1].SetActive(true);
                 gObjHealthBis[indexButtonCat - 1].SetActive(true);
-               
+
             }
 
             //Player's Mana
             if (categoryDropdown.value == 2)
             {
-                
+
                 initLine(indexButtonCat - 1);
 
                 switchCatCount++;
                 gObjMana[indexButtonCat - 1].SetActive(true);
                 gObjManaBis[indexButtonCat - 1].SetActive(true);
-               
+
             }
 
             //Enemy's Health
             if (categoryDropdown.value == 3)
             {
-                
 
+                gObjHealthEn[indexButtonCat - 1].SetActive(true);
+                gObjHealthBisEn[indexButtonCat - 1].SetActive(true);
                 switchCatCount++;
             }
 
             //Enemy's Mana
             if (categoryDropdown.value == 4)
             {
-                
+                gObjManaEn[indexButtonCat - 1].SetActive(true);
+                gObjManaBisEn[indexButtonCat - 1].SetActive(true);
                 switchCatCount++;
             }
 
@@ -155,9 +181,9 @@ public class IASceneManager : MonoBehaviour
             if (categoryDropdown.value == 0)
             {
                 initLine(indexButtonCat - 1);
-                
+
                 switchCatCount++;
-                
+
             }
 
 
@@ -167,7 +193,15 @@ public class IASceneManager : MonoBehaviour
                 //TODO print textToChoose to the screen
             }
         }
-    }
+    
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }   
 
 
     

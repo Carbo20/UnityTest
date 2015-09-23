1a7d6f4fa6589376ce4217212f16eb4a2d9b569e
carbo20 <carbo20@hotmail.fr>
2015-09-23 15:30:34 +0200

    Ajout d'une classe abstact MonsterStatus Mere de tous les script Monstre

--- Assets/Scripts/Classes/MonsterStatus.cs
+++ Assets/Scripts/Classes/MonsterStatus.cs
@@ -2,6 +2,255 @@
 using System.Collections;
 
 public abstract class MonsterStatus : MonoBehaviour {
+    
+    private int hpMax;
+    private int manaMax;
+    private int hp;
+    private int mana;
 
-	
+    private int regenHp;
+    private int regenMana;
+
+    private int armor;
+
+    private int strenght;
+    private int intelligence;
+    private int agility;
+    private int vitality;
+
+    private int speed;
+    private int damage;
+    private int spellDamage;
+    private float dodge;
+    private float critical;
+
+    private float cdAttack;
+
+    /* We have to put the IA monster into DoAnAction wich choose wich action
+    the monster will do --------------------------------------------------*/
+    public abstract Data.SkillType DoAnAction();
+
+
+    /* Getter and Setter */
+
+    public int HpMax
+    {
+        get
+        {
+            return hpMax;
+        }
+
+        set
+        {
+            hpMax = value;
+        }
+    }
+
+    public int ManaMax
+    {
+        get
+        {
+            return manaMax;
+        }
+
+        set
+        {
+            manaMax = value;
+        }
+    }
+
+    public int Hp
+    {
+        get
+        {
+            return hp;
+        }
+
+        set
+        {
+            hp = value;
+        }
+    }
+
+    public int Mana
+    {
+        get
+        {
+            return mana;
+        }
+
+        set
+        {
+            mana = value;
+        }
+    }
+
+    public int RegenHp
+    {
+        get
+        {
+            return regenHp;
+        }
+
+        set
+        {
+            regenHp = value;
+        }
+    }
+
+    public int RegenMana
+    {
+        get
+        {
+            return regenMana;
+        }
+
+        set
+        {
+            regenMana = value;
+        }
+    }
+
+    public int Armor
+    {
+        get
+        {
+            return armor;
+        }
+
+        set
+        {
+            armor = value;
+        }
+    }
+
+    public int Strenght
+    {
+        get
+        {
+            return strenght;
+        }
+
+        set
+        {
+            strenght = value;
+        }
+    }
+
+    public int Intelligence
+    {
+        get
+        {
+            return intelligence;
+        }
+
+        set
+        {
+            intelligence = value;
+        }
+    }
+
+    public int Agility
+    {
+        get
+        {
+            return agility;
+        }
+
+        set
+        {
+            agility = value;
+        }
+    }
+
+    public int Vitality
+    {
+        get
+        {
+            return vitality;
+        }
+
+        set
+        {
+            vitality = value;
+        }
+    }
+
+    public int Speed
+    {
+        get
+        {
+            return speed;
+        }
+
+        set
+        {
+            speed = value;
+        }
+    }
+
+    public int Damage
+    {
+        get
+        {
+            return damage;
+        }
+
+        set
+        {
+            damage = value;
+        }
+    }
+
+    public int SpellDamage
+    {
+        get
+        {
+            return spellDamage;
+        }
+
+        set
+        {
+            spellDamage = value;
+        }
+    }
+
+    public float Dodge
+    {
+        get
+        {
+            return dodge;
+        }
+
+        set
+        {
+            dodge = value;
+        }
+    }
+
+    public float Critical
+    {
+        get
+        {
+            return critical;
+        }
+
+        set
+        {
+            critical = value;
+        }
+    }
+
+    public float CdAttack
+    {
+        get
+        {
+            return cdAttack;
+        }
+
+        set
+        {
+            cdAttack = value;
+        }
+    }
 }
--- Assets/Scripts/Monster/Mummy.cs
+++ Assets/Scripts/Monster/Mummy.cs
@@ -3,5 +3,32 @@ using System.Collections;
 
 public class Mummy : MonsterStatus {
 
-	
+    public Mummy()
+    {
+        HpMax = 10;
+        ManaMax = 30;
+
+        RegenHp = 2;
+        RegenMana = 0;
+
+        Strenght = 3;
+        Agility = 1;
+        Intelligence = 7;
+        Vitality = 7;
+
+        Speed = 3;
+        Damage = 5;
+        SpellDamage = 7;
+        Dodge = 0;
+        Critical = 0;
+
+        CdAttack = 1.5f;
+    }
+
+    public override Data.SkillType DoAnAction()
+    {
+        Data.SkillType idSkill;
+        idSkill = Data.SkillType.ATTACK;
+        return idSkill;
+    }
 }

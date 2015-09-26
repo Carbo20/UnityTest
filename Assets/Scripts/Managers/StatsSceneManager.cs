78008ad98ab61d078b0c3d73e64168e5dd6824a4
Cricroo <crizz13@hotmail.fr>
2015-09-26 21:54:27 +0200

    stats

---
+++ Assets/Scripts/Managers/StatsSceneManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatsSceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    public void CallbackButtonContinue()
+    {
+        //Switch the current scene to the menu
+        Application.LoadLevel("loadingScene");
+    }
+
+
+    void SaveStats()
+    {
+
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
